3b56a3ea29ed6ed041d30809795911b406397cde
Julian Tauert <julian.tauert@gmail.com>
2020-07-20 22:32:37 +0200

    Set default values for slider component

--- src/Slider.js
+++ src/Slider.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 function Slider(props) {
-  const { value, min, max } = props
+  const { value = 3000, min = 3000, max = 9999, step = 1, onSliderChange } = props
   
-  return <input type="range" min={min} max={max} value={value} onChange={props.onSliderChange} step={props.step} />
+  return <input type="range" min={min} max={max} value={value} onChange={onSliderChange} step={step} />
 }
 
 export default Slider
