ca1e91cd67f41369ea09fd2694777065b60ddb14
Julian Tauert <julian.tauert@9elements.com>
2020-07-25 00:19:07 +0200

    Refactor clasc component to function component

--- src/App.js
+++ src/App.js
@@ -1,63 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Slider from "./Slider"
 import TotalInput from "./TotalInput"
 import PriceRow from "./PriceRow"
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      total: 3000,
-      multiplicator: 0.66,
-      vatrate: 0.16
-    }
-    this.handleChange = this.handleChange.bind(this)
-    this.calculateDeposit = this.calculateDeposit.bind(this)
-    this.calculateRate = this.calculateRate.bind(this)
-    this.roundValue = this.roundValue.bind(this)
+function App() {
+  const [total, setTotal] = useState(3000)
+  const [multiplicator, setMultiplicator] = useState(0.66)
+  const [vatrate, setVatrate] = useState(0.16)
+  const [deposit, setDeposit] = useState(calculateDeposit(total))
+  const [weeklyRate, setWeeklyRate] = useState(calculateDeposit(total))
+
+  function calculateDeposit(total) {
+    return roundValue(total * (vatrate + 1) * 0.1)
   }
 
-  componentDidMount() {
-    this.setState({
-      deposit: this.calculateDeposit(this.state.total),
-      rate: this.calculateRate(this.state.total)
-    })
+  function calculateRate(total) {
+    return roundValue(total * multiplicator / 52)
   }
 
-  calculateDeposit(total) {
-    return this.roundValue(total * (this.state.vatrate + 1) * 0.1)
-  }
-
-  calculateRate(total) {
-    return this.roundValue(total * this.state.multiplicator / 52)
-  }
-
-  roundValue(value) {
+  function roundValue(value) {
     return value.toFixed(2)
   }
 
-  handleChange(event) {
+  function handleChange(event) {
     const { value } = event.target
 
-    this.setState({
-      total: value,
-      deposit: this.calculateDeposit(value),
-      rate: this.calculateRate(value)
-    })
+    setTotal(value)
+    setDeposit(calculateDeposit(value))
+    setWeeklyRate(calculateRate(value))
   }
 
-  render() {
-    return (
-      <>
-        <Slider min={300} max={9999} value={this.state.total} step={1} onSliderChange={this.handleChange} />
-        <PriceRow label="Gesamtpreis (netto)" value={this.state.total} />
-        <TotalInput value={this.state.total} onInputChange={this.handleChange} />
-        <PriceRow label="Einmalige Kaution" value={this.state.deposit} />
-        <PriceRow label="Wöchenlichte Rate (zzgl. MwSt)" value={this.state.rate} />
-      </>
-    );
-  }
+  return (
+    <>
+      <Slider min={300} max={9999} value={total} step={1} onSliderChange={handleChange} />
+      <PriceRow label="Gesamtpreis (netto)" value={total} />
+      <TotalInput value={total} onInputChange={handleChange} />
+      <PriceRow label="Einmalige Kaution" value={deposit} />
+      <PriceRow label="Wöchenlichte Rate (zzgl. MwSt)" value={weeklyRate} />
+    </>
+  )
 }
 
 export default App;
